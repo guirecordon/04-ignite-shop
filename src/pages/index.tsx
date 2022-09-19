b47e91c36ed12355258fc56d1cdfa8ae8ea49721
Gui Recordon <guilherme.recordon@gmail.com>
2022-09-19 18:51:44 -0300

    Style home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,40 @@
-import { styled } from '../styles'
+import Image from 'next/future/image';
+import { HomeContainer, Product } from '../styles/pages/home';
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-})
+import camiseta1 from '../assets/camisetas/1.png';
+import camiseta2 from '../assets/camisetas/2.png';
+import camiseta3 from '../assets/camisetas/3.png';
 
 export default function Home() {
-  return <Button>Hello, world!</Button>;
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} alt="camiseta 1" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} alt="camiseta 1" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta3} alt="camiseta 1" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+    </HomeContainer>
+    );
 }
