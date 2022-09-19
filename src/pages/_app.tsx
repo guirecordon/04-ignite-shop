7d96d9ddd536be0bc148fa544fb76431325d1a79
Gui Recordon <guilherme.recordon@gmail.com>
2022-09-19 16:52:57 -0300

    Add global styles and header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,20 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import { AppProps } from "next/app";
+import { globalStyles } from "../styles/global";
+import igniteLogo from '../assets/igniteLogo.svg';
+import Image from "next/image";
+import { Container, Header } from "../styles/pages/app";
 
-export default MyApp;
+globalStyles();
+
+export default function App({ Component, pageProps }: AppProps) {
+  
+  return ( 
+    <Container>
+      <Header>
+        <Image src={igniteLogo} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
+}
