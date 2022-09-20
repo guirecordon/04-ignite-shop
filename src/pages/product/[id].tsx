1c637b668ea1e0c496fb2297d0fc0ce1789bc388
Gui Recordon <guilherme.recordon@gmail.com>
2022-09-20 15:17:57 -0300

    Create product page styles

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,26 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
 export default function Product() {
   const { query } = useRouter()
   
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum eligendi deleniti laboriosam officia magni aut ullam? Doloremque hic eveniet pariatur, corrupti voluptas deserunt vel illum harum repellendus quod laborum veritatis.
+        </p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
