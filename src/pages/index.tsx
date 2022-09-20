bb1434393d4d15cb0f2ec5687548b3f08e662897
Gui Recordon <guilherme.recordon@gmail.com>
2022-09-20 16:34:42 -0300

    Set prefetch to false on link of home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => {
         return (
-          <Link key={product.id} href={`/product/${product.id}`}>
+          <Link key={product.id} href={`/product/${product.id}`} prefetch={false}>
             <Product  className="keen-slider__slide">
               <Image src={product.imageUrl} alt="camiseta 1" width={520} height={480} />
 
