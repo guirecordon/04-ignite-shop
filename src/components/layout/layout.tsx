164aaf2a6a9f1157ac9741ad8e5bea3b96cf251b
Gui Recordon <guilherme.recordon@gmail.com>
2022-09-29 15:12:19 -0300

    Add cart icon increment logic

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -3,12 +3,14 @@ import { Handbag } from "phosphor-react";
 import { CartIconContainer, Container, Header } from "../../styles/pages/app";
 import igniteLogo from '../../assets/igniteLogo.svg';
 import ModalCartShop from "../modalCartShop";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { CartCounter } from "../../styles/components/layout";
+import { CartItemsContext } from "../../contexts/CartItemsContext";
 
 
 export default function Layout({children}) {
   const [isOpen, setIsOpen] = useState(false);
+  const { cartItems } = useContext(CartItemsContext);
 
   function toggleCart() {
     setIsOpen(!isOpen);
@@ -23,7 +25,8 @@ export default function Layout({children}) {
 
         <CartIconContainer onClick={toggleCart}>
           <Handbag size={24} weight="bold" color="#8D8D99"/>
-          <CartCounter>1</CartCounter>
+          {cartItems.length > 0 &&  <CartCounter>{cartItems.length}</CartCounter>}
+          
         </CartIconContainer>
         
       </Header>
